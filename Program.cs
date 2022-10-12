3019307fe02d3309e3756fb577f17a74b3fdb1f6
saif-ulhussain <saif-ul.hussain@sainsburys.co.uk>
2022-10-12 17:28:45 +0100

    Inital Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Example
+{
+    static void Main(string[] args)
+    {
+        var FavouriteSport = new List<string>
+        {
+            "Football",
+            "Tennis",
+            "Rugby"
+        };
+
+        FavouriteSport.Add("Snooker");
+        FavouriteSport.Remove("Rugby");
+
+        foreach (string sport in FavouriteSport)
+        {
+            Console.WriteLine(sport);
+        }
+
+    }
+}
